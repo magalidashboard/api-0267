d5ec444257fc6d35a7988f203568244ef6727567
LucasHDSF <lucashiago.dsf@gmail.com>
2022-09-18 10:18:18 -0300

    save in database all webhooks notification

---
+++ src/services/serviceNotification.js
@@ -0,0 +1,26 @@
+const database = require('../config/database');
+const modelCaller = require('../database/models/modelNotification');
+const fs = require('fs');
+const global = require('../main');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+
+exports.Create = async (
+    notification
+) => {
+    try {
+
+        const created = await modelCaller.create(
+            notification
+        )
+
+        return created;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
